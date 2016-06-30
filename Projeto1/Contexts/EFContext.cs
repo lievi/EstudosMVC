0647411ab57d084440504eb68bd8181bf314675b
lievi <lievi.silva@gmail.com>
2016-06-30 14:43:55 -0300

    Criação de contexto com Entity Framework e conexão com banco do Azure

---
+++ Projeto1/Contexts/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Projeto1.Models;
+using System.Data.Entity;
+
+namespace Projeto1.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+
+        public DbSet<Categoria> Categorias { get;set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
