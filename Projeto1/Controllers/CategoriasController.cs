7f7352ea7b5b10d5a0ff1aab2ccdaf8aee7cb5e3
lievi <lievi.silva@gmail.com>
2016-06-30 00:18:37 -0300

    Add project files.

---
+++ Projeto1/Controllers/CategoriasController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Projeto1.Models;
+
+namespace Projeto1.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        public static IList<Categoria> categorias = new List<Categoria>()
+        { new Categoria(1,"Notebooks"),new Categoria(2,"Monitores"),new Categoria(3,"Impressoras"),new Categoria(4,"Mouses"),new Categoria(5,"Desktops")};
+
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            return View(categorias);
+        }
+    }
+}
---
+++ Projeto1/Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto1.Models
+{
+    public class Categoria
+    {
+        public long CategoriaID { get; set; }
+        public string Nome { get; set; }
+
+        public Categoria(long categoriaID, string nome)
+        {
+            this.CategoriaID = categoriaID;
+            this.Nome = nome;
+        }
+    }
+}
